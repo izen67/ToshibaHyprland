2bc3ada08a0636eca3d52489458faa94d77c0fe2
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-17 13:00:58 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,20 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # end-4 dots for NixOS (QuickShell variant)
+    illogical-impulse = {
+      url = "github:xBLACKICEx/end-4-dots-hyprland-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, illogical-impulse, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      # specialArgs makes `inputs` available to every module
+      # make `inputs` visible inside all modules
       specialArgs = { inherit inputs; };
 
       modules = [
         ./configuration.nix
+
+        # Home-Manager as a NixOS module (import it here, not again in configuration.nix)
         home-manager.nixosModules.default
       ];
     };
