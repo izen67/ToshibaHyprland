79b056e733ac78732c2112f1f34c347a87faa84e
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-17 13:15:49 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,8 +1,10 @@
 { config, pkgs, inputs, ... }:
 
+let
+  # resolve the flake's package set for the current system
+  illogicalPkgs = inputs.illogical-impulse.packages.${pkgs.system};
+in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "toshiba";
   home.homeDirectory = "/home/toshiba";
 
@@ -11,91 +13,24 @@
   ];
 
   illogical-impulse = {
-  enable = true;
+    enable = true;
 
-  hyprland = {
-    # Use the customized Hyprland build provided by the dots
-    package = hypr.hyprland;
-    xdgPortalPackage = hypr.xdg-desktop-portal-hyprland;
+    hyprland = {
+      # Explicitly use the package set exported by the dots flake
+      package = illogicalPkgs.hypr.hyprland;
+      xdgPortalPackage = illogicalPkgs.hypr.xdg-desktop-portal-hyprland;
 
-    ozoneWayland.enable = true;
+      ozoneWayland.enable = true;
+    };
+
+    dotfiles = {
+      fish.enable = true;
+      kitty.enable = true;
+    };
   };
 
-  dotfiles = {
-    fish.enable = true;
-    kitty.enable = true;
-  };
- };
-
-# Make sure Fish is actually enabled at HM level too (safe to keep even if the module also does it)
-programs.fish.enable = true;
-
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/izen/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
+  programs.fish.enable = true;
   programs.home-manager.enable = true;
+
+  home.stateVersion = "25.05";
 }
