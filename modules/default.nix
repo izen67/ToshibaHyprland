0cf7f65c556a1455e486d50fbb775fe9df0fb8ca
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-17 12:59:04 +0300

    Delete modules directory

--- modules/default.nix
+++
@@ -1,9 +0,0 @@
-self: illogical-impulse-dotfiles: inputs: {... }:
-{
-  imports = [
-    (import ./options.nix illogical-impulse-dotfiles)
-    (import ./quickshell.nix illogical-impulse-dotfiles inputs)
-    (import ./hyprland.nix illogical-impulse-dotfiles inputs)
-    (import ./packages.nix inputs)
-  ];
-}
